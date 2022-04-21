9b7c4020a75d79ebb6fe6551e58af68c528c8612
GitHub Action <action@github.com>
2022-04-21 20:31:29 +0000

    Template cleanup

---
+++ src/main/kotlin/com/github/josephkrupinsky/testplugin/services/MyApplicationService.kt
@@ -0,0 +1,10 @@
+package com.github.josephkrupinsky.testplugin.services
+
+import com.github.josephkrupinsky.testplugin.MyBundle
+
+class MyApplicationService {
+
+    init {
+        println(MyBundle.message("applicationService"))
+    }
+}
